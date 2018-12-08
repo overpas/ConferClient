4580e9d41aca65e4f08b978925631eedac2de02c
overpas <pckeycalculator@gmail.com>
2018-12-08 22:57:49 +0300

    Initial commit

---
+++ app/src/main/java/by/overpass/conferclient/ConferApp.kt
@@ -0,0 +1,20 @@
+package by.overpass.conferclient
+
+import android.app.Application
+import android.content.Context
+import timber.log.Timber
+
+class ConferApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Timber.plant(Timber.DebugTree())
+    }
+
+    companion object {
+        private lateinit var instance: ConferApp
+        fun getAppContext(): Context = instance.applicationContext
+    }
+
+}
